1d975cfcbb8ef9c38b47b014c5e06f33fc0b6ded
RiddleASL <n00212142@iadt.ie>
2023-11-23 01:11:56 +0000

    prepare for war

--- college-api/tailwind.config.js
+++ college-api/tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
+	content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	theme: {
+		extend: {
+      colors: {
+        'primary': '#C4D4F2',
+      },
+    },
+	},
+	plugins: [],
+};
